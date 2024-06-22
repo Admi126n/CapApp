26bf1b0169152a3f61222417337467694193750e
MISTKNNN <arek.pytka2812@gmail.com>
2024-06-22 21:41:37 +0200

    [SETUP] Basic backend project setup

---
+++ Backend/src/main/java/com/capapp/backend/CapAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.capapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapAppBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/capapp/backend/CapAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
