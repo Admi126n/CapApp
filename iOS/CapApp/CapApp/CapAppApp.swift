c599305a19c0bc683303776a1e109f6da324b396
Adam Tokarski <a.tokarski126@gmail.com>
2024-06-22 12:55:44 +0200

    Add iOS project

---
+++ iOS/CapApp/CapApp/CapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapAppApp.swift
+//  CapApp
+//
+//  Created by Adam Tokarski on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
